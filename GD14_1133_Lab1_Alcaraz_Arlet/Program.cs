4eb652eaa213b6c8853e093ce15486d1699713e5
Arlo <lux.morte@yahoo.com>
2025-09-05 20:53:17 -0700

    Exploring Visual Studio
    Basic project with an extra line

---
+++ GD14_1133_Lab1_Alcaraz_Arlet/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace GD14_1133_Lab1_Alcaraz_Arlet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Arlet Alcaraz ENTA 1133");
+        }
+    }
+}
