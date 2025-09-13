aba0f6419bf44e7977ae285717570d3fe4b84fa4
Arlo <lux.morte@yahoo.com>
2025-09-12 21:51:26 -0700

    Created the new project
    Added the new project for the dice game

---
+++ Dice Game/GD14_1133_Dice_Game_Alcaraz_Arlet/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GD14_1133_Dice_Game_Alcaraz_Arlet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
