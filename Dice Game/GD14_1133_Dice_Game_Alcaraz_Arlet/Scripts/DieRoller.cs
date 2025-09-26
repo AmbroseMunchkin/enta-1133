6d6da8242cdccb3d192e2e40b2876c8f54f847d3
Arlo <lux.morte@yahoo.com>
2025-09-25 18:11:46 -0700

    Added a Player script
    Added a player script, changed the DieRoller so it lets the player chose the dice

--- Dice Game/GD14_1133_Dice_Game_Alcaraz_Arlet/Scripts/DieRoller.cs
+++ Dice Game/GD14_1133_Dice_Game_Alcaraz_Arlet/Scripts/DieRoller.cs
@@ -8,21 +8,62 @@ namespace GD14_1133_Dice_Game_Alcaraz_Arlet.Scripts
 {
     internal class DieRoller
     {
-        public static void Rolls()
+
+        public static void PlayerRolls()
         {
             Random random = new Random();
-            int d6 = random.Next(1, 7);
-            int d8 = random.Next(1, 9); //Was struggling to get the random numbers working, until i saw i needed to open my public void so it could be called in the GameManager
-            int d12 = random.Next(1, 13);
-            int d20 = random.Next(1, 21);
+            int die4 = random.Next(1, 5);
+            int die6 = random.Next(1, 7);
+            int die8 = random.Next(1, 9); 
+            int die12 = random.Next(1, 13);
+            int die20 = random.Next(1, 21);
 
-            Console.WriteLine($"Your d6 landed in: {d6}");
-            Console.WriteLine($"Your d8 landed in: {d8}"); 
-            Console.WriteLine($"Your d12 landed in: {d12}");
-            Console.WriteLine($"Your d20 landed in: {d20}");
+            Console.WriteLine("Pick your dice!");
+            Console.WriteLine("You have a d4 / d6 / d8 / d12 / d20");
             Console.WriteLine();
-            Console.WriteLine($"Your total is: {d6 + d8 + d12 + d20}");
-            Console.WriteLine();
+            Console.WriteLine("Type the dice you want to use:");
+            string choiceInput = Console.ReadLine();
+            if (choiceInput == "d4")
+            {
+                Console.WriteLine("You chose a D4");
+                Console.WriteLine("It landed in a " + die4 + "!");
+            }
+            else if (choiceInput == "d6")
+            {
+                Console.WriteLine("You chose a D6");
+                Console.WriteLine("It landed in a " + die6 + "!");
+            }
+            else if (choiceInput == "d8")
+            {
+                Console.WriteLine("You chose a D8");
+                Console.WriteLine("It landed in a " + die8 + "!");
+            }
+            else if (choiceInput == "d12")
+            {
+                Console.WriteLine("You chose a D12");
+                Console.WriteLine("It landed in a " + die12 + "!");
+            }
+            else if (choiceInput == "d20")
+            {
+                Console.WriteLine("You chose a D20");
+                Console.WriteLine("It landed in a " + die20 + "!");
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice, using the d6 as default");
+                Console.WriteLine("It landed in a " + die6 + "!");
+            }
+        }
+        public static void ComputerRolls() //It was easier for me to have the dice rolling for the player and the computer in different public voids
+        {
+            Random random = new Random();
+            int die4 = random.Next(1, 5);
+            int die6 = random.Next(1, 7);
+            int die8 = random.Next(1, 9);
+            int die12 = random.Next(1, 13);
+            int die20 = random.Next(1, 21);
+
+            Console.WriteLine("Computer chose a D4 and got " + die4);
         }
     }
 }
---
+++ Dice Game/GD14_1133_Dice_Game_Alcaraz_Arlet/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GD14_1133_Dice_Game_Alcaraz_Arlet.Scripts
+{
+    internal class Player
+    {
+        public static void User()
+        {
+           
+            Console.Write("Please enter your username:");
+            String username = Console.ReadLine();
+            Console.WriteLine("Welcome " + username + " to Dice Rolling! My name is Arlet Alcaraz and today is 25 of september 2025");
+        }
+    }
+}
